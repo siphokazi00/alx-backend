444360df8105650ece09701b0b6fd197babf9b2f
siphokazi <usphow@gmail.com>
2024-09-22 21:46:17 +0000

    README

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Renders the index page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
